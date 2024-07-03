87b9cdeba53968a3c6be2521d285de9b8bf9b0ed
igorf08 <igorfmaa@gmail.com>
2024-07-03 19:05:46 -0300

    Fixing module error

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-import express from "express";
-
+const fs = require('fs');
+const express = require('express');
 const app = express();
 
-const port = 3000
+const port = 3000;
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.listen(port, () => {
   console.log(`Im working at port: ${port}.`);
 });
 
+//Route Handler
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours
+    },
+  });
+});
